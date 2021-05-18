cee381d1fc6e6648b816f31151b70cba521ea8b6
Lisa <lisanour13@gmail.com>
2021-05-18 14:55:34 +0200

    creation index

---
+++ phpManga/resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<table class= "table table-bordered table-striped">
+    <thread>
+        <th> Id </th>
+        <th> Titre </th>
+        <th> Prix </th>
+        <th> Couverture </th>
+    </thread>
+    @foreach ($mangas as $manga)
+        <tr>
+            <td> {{$manga ->id_manga}}</td>
+            <td> {{$manga ->titre}}</td>
+            <td> {{$manga ->prix}}</td>
+            <td> {{$manga ->couverture}}</td>
+        </tr>
+</table>
