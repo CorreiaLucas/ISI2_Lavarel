0ec8693712b83aa12661c3c73ae653e77ecd7a42
lucas <lu.correia71@gmail.com>
2021-06-08 16:43:01 +0200

    adding form

---
+++ phpORM/resources/views/monFormulaire.blade.php
@@ -0,0 +1,10 @@
+@extends('Layout')
+
+@section('contenu')
+    <form action = "{{url('testformulaire')}}" method="POST">
+        @csrf
+        <label for="message"> Entrez votre message : </label>
+        <input type="text" name="message" id="message">
+        <input type="submit" value="Envoyer !">
+    </form>
+@endsection
